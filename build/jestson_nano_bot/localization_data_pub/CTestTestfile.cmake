6e769b8650e21ab34ac40dd7f73c01c45f68e984
ManhBeo0101 <manhta01012001@gmail.com>
2023-04-25 17:33:57 +0700

    manh

---
+++ build/jestson_nano_bot/localization_data_pub/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/catkin_ws/src/jestson_nano_bot/localization_data_pub
+# Build directory: /home/pi/catkin_ws/build/jestson_nano_bot/localization_data_pub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
